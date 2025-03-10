e228e0df9c323cc4742a35a9179d1135bcae1a6c
ThanosSkentzos <kalligerist@gmail.com>
2025-03-10 11:14:07 +0100

    results

--- 1_rmi_bechmarking/run-all.sh
+++ 1_rmi_bechmarking/run-all.sh
@@ -4,7 +4,7 @@ sequenceNumberCalls=100000
 
 rm -f out.* err.*
 
-for size in 2 3 5 9 13 16
+for size in $(seq 2 16)
 do
   echo running size $size
   srun -N $size run.sh $sequenceNumberCalls > out.$size 2> err.$size
