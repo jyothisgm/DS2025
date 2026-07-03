92edbabe3f3999ddf14d0be04835deba4046806d
ThanosSkentzos <kalligerit@gmail.com>
2026-07-03 15:11:26 +0200

    repeated experiments

--- 1_rmi_benchmarking/run-all.sh
+++ 1_rmi_benchmarking/run-all.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 sequenceNumberCalls=100000
-
+reps=5
+sh build.sh
 rm -f out.* err.*
 
-for size in $(seq 2 10)
+
+for n in $(seq 1 $reps)
 do
-  echo running size $size
-  srun -N $size run.sh $sequenceNumberCalls > out.$size 2> err.$size
+  for size in $(seq 2 11)
+  do
+    echo running size $size
+    srun -N $size run.sh $sequenceNumberCalls > out.$size 2> err.$size
+  done
+
+  # set header
+  if [ $n -eq 1 ]
+  then
+    cat out.* | grep Clients | head -n 1 > results_ib.csv
+  fi
+  # gather results to file
+  sh ib_gather.sh
+  # cleanup
+  rm -f out.* err.*
 done
